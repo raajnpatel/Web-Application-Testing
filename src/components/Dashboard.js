eb894646f9c9ca99a833c37beeaff6919b48c0dc
Raajn Patel <rndpatel@gmail.com>
2019-09-05 09:21:13 -0700

    added more functionality to the dashboard. added strike and ball

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Display from "./Display";
 
 const Dashboard = () => {
+    const [strike, setStrike] = useState(0);
+    const [balls, setBalls] = useState(0);
+
+    function strikeScore() {
+        return setStrike(strike + 1);
+    }
+    function ballScore() {
+        return setBalls(balls + 1);
+    }
+
     return (
         <div>
-        Add some buttons
-    <Display />
-    </div>
-);
+            Add some buttons
+            <Display />
+            <button onClick={strikeScore}>STRIKE</button>
+            <button onClick={ballScore}>BALL</button>
+        </div>
+    );
 };
 
 export default Dashboard;
