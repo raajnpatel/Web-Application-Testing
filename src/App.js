6dc858e2aa1b0eab98ac13e3cac2518fac6ca00b
Raajn Patel <rndpatel@gmail.com>
2019-09-05 09:29:24 -0700

    added visual representation of balls and strikes without keeping track of number.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import React from 'react';
-import './App.css';
-import Dashboard from "./components/Dashboard";
 
-function App(){
+import React, { useState } from "react";
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import Display from "./components/Display";
+
+function App() {
+  const [strike, setStrike] = useState(0);
+  const [balls, setBalls] = useState(0);
+  const [score, setScore] = useState(0);
+
+  const actions = {
+    StrikeUp: () => {
+      if (strike < 2) {
+        return setStrike(strike + 1);
+      } else {
+        return setStrike(0), setBalls(0);
+      }
+    },
+    BallsUp: () => {
+      if (balls < 3) {
+        return setBalls(balls + 1);
+      } else {
+        return setStrike(0), setBalls(0);
+      }
+    }
+  };
+
   return (
-      <div className = "App">
-        Hello <Dashboard/>
+      <div className="App">
+        <h1>Play the game</h1>
+        <Dashboard play={actions} />
+        <Display strike={strike} balls={balls} />
       </div>
-  )
+  );
 }
 
 export default App;
